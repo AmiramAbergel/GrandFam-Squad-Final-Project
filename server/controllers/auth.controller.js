85b3618459afa568d87a7f5f5a22312a9d2173be
unknown <Amiram.ab@gmail.com>
2022-12-23 16:17:56 +0200

    catchAsync func added in order to wrap any asnyc function with try-catch

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+import { User } from '../models/user.model.js';
+
+export const signup = async (req, res, next) => {
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm,
+    });
+};
