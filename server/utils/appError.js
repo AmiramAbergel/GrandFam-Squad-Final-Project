b43bd4940b7a26d84dec8ee3703ffb064a5aa3df
unknown <Amiram.ab@gmail.com>
2022-12-23 17:55:52 +0200

    start with error controller to migrate all errors in the app to one controller that will manage it

--- server/utils/appError.js
+++ server/utils/appError.js
@@ -1,9 +1,9 @@
 function AppError(message, statusCode) {
-    const error = new Error(message);
+    const error = new Error(message); // Create a new Error Object
 
-    error.statusCode = statusCode;
-    error.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-    error.isOperational = true;
+    error.statusCode = statusCode; // 404, 500, etc.
+    error.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error'; // 4xx is a client error, 5xx is a server error
+    error.isOperational = true; // This is an operational error, not a programming error
 
     return error;
 }
