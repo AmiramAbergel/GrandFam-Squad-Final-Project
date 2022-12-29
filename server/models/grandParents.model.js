db1b85eec4693fda64377426c057aab24e2b29ee
unknown <Amiram.ab@gmail.com>
2022-12-29 10:35:14 +0200

    grandparents model addded

--- server/models/grandParents.model.js
+++ server/models/grandParents.model.js
@@ -2,6 +2,23 @@ import mongoose from 'mongoose';
 import validator from 'validator';
 const { Schema } = mongoose;
 
-const grandParentsSchema = new Schema({});
+const grandParentsSchema = new Schema({
+    side: { type: String, required: true },
+    grandma: {
+        name: { type: String, required: true },
+        age: { type: Number, required: true },
+    },
+    spouse: {
+        name: { type: String, required: true },
+        age: { type: Number, required: true },
+        relationship: { type: String, required: true },
+    },
+    familyName: { type: String, required: true },
+    sharedWith: [{ type: String, required: true }],
+    address: { type: String, required: true },
+    phone: { type: String, required: true },
+    img: { type: String, required: true, default: '' },
+    location: { type: { type: String }, coordinates: [Number] },
+});
 
 export const GrandParents = mongoose.model('GrandParents', grandParentsSchema);
